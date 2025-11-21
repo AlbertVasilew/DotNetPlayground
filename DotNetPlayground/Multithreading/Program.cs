46d81f4b20208ac56029d59b2dbb027b89860be5
Albert Vasilev <avasilev2260@gmail.com>
2025-11-21 11:08:05 +0200

    Offloading main thread in integer generators

--- DotNetPlayground/Multithreading/Program.cs
+++ DotNetPlayground/Multithreading/Program.cs
@@ -1,19 +1,28 @@
 ﻿using Multithreading.DivideAndConquer;
-
-// [SYNC] is using only 1 thread (main)
+using System.Diagnostics;
 
 var syncGenerating = new IntegerGenerator(1000000000);
-
-Console.WriteLine("[SYNC]");
-Console.WriteLine($"Generated Integers: {syncGenerating.GenerateSync()}");
-Console.WriteLine($"Sum Generated: {syncGenerating.SumGeneratedSync()}");
-
-Console.WriteLine("----------");
-
-// [ASYNC] is using as many threads as the processor cores are
-
 var asyncGenerating = new IntegerGenerator(1000000000);
 
-Console.WriteLine("[ASYNC]");
-Console.WriteLine($"Generated Integers [SYNC]: {asyncGenerating.GenerateAsync()}");
-Console.WriteLine($"Sum Generated [SYNC]: {asyncGenerating.SumGeneratedAsync()}");
+var stopwatch = Stopwatch.StartNew();
+
+var syncTask = Task.Run(() =>
+{
+    var count = syncGenerating.GenerateSync();
+    Console.WriteLine($"[SYNC] Integers: {count}");
+    var sum = syncGenerating.SumGeneratedSync();
+    Console.WriteLine($"[SYNC] Sum: {sum}");
+});
+
+var asyncTask = Task.Run(async () =>
+{
+    var count = asyncGenerating.GenerateAsync();
+    Console.WriteLine($"[ASYNC] Integers: {count}");
+    var sum = asyncGenerating.SumGeneratedAsync();
+    Console.WriteLine($"[ASYNC] Sum: {sum}");
+});
+
+await Task.WhenAll(syncTask, asyncTask);
+
+Console.WriteLine("Both generations completed in parallel (offload long running tasks)");
+Console.WriteLine($"Estimation Time: {stopwatch.ElapsedMilliseconds}");
