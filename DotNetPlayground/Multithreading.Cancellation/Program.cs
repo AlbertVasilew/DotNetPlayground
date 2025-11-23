d21f10c75bd6fb5f0f7ab9028e72d51970c26999
Albert Vasilev <avasilev@gmail.com>
2025-11-23 12:18:31 +0200

    Thread cancellation

---
+++ DotNetPlayground/Multithreading.Cancellation/Program.cs
@@ -0,0 +1,22 @@
+﻿var cts = new CancellationTokenSource();
+
+var thread = new Thread(() =>
+{
+    while (!cts.Token.IsCancellationRequested)
+    {
+        Console.WriteLine("Working...");
+        Thread.Sleep(1000);
+    }
+
+    Console.WriteLine("Cancellation requested by user");
+});
+
+Console.WriteLine("Press C to cancel the operation");
+
+thread.Start();
+
+if (Console.ReadKey().Key == ConsoleKey.C)
+    cts.Cancel();
+
+thread.Join();
+Console.WriteLine("Program finished");
